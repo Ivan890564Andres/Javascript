2302ac316c94e3f4998223ce50ff603c50c5ffa6
Ivan890564Andres <ivan890564saavedra@gmail.com>
2020-10-08 11:16:39 -0300

    arquivo main.js linkado ao index.html
    No arquivo main.js aparecem alguns comandos basicos de javascript e alguns comentarios explicativos sobre alguns comandos importantes e situações relevantes

---
+++ main.js
@@ -0,0 +1,15 @@
+alert("Meu primeiro aviso de alerta escrito dentro de um arquivo tipo js")
+
+var nome = "Ivan Andrés Saavedra";
+// comenta somente uma linha
+/* comenta várias linhas */
+// tipagem dinamica: não precisa declarar previamente o tipo da variavel.
+/* somente pelo fato de que o texto foi colocado entre aspas duplas o Javascript percebe
+   que o tipo da variavel é texto ou string */
+
+alert("Bem vindo Prezado Visitante  " + nome) // simbolo + pode ser usado para concatenar strings
+
+var idade=29;
+// 	alert ("Meu segundo aviso de alerta num arquivo js");
+// 	alert ("Bem Vindo:  " + nome); // serve para comentar uma linha no Javascript
+  alert (nome + " tem " + idade + " anos " );
